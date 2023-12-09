90b75c2550612bd09d0abc6dc2a6bfcaff039407
Alex Sáez <alexsaezoses@gmail.com>
2023-12-09 01:59:59 -0300

    Commit Controlador: "UsuarioManager"

---
+++ src/main/java/Controladores/UsuarioManager.java
@@ -0,0 +1,42 @@
+package Controladores;
+
+import Modelo.*;
+import java.io.*;
+
+public class UsuarioManager {
+    public static final String nombreArchivoUsuarios = "Usuarios.csv";
+
+    public static boolean verificarAutenticacion(String rut, String contraseña) {
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(nombreArchivoUsuarios));
+            String linea;
+
+            boolean primeraLinea = true;
+
+            while ((linea = lector.readLine()) != null) {
+                if (primeraLinea) {
+                    primeraLinea = false;
+                    continue;
+                }
+
+                String[] campos = linea.split(",");
+
+                if (campos.length >= 2) {
+                    String rutUsuario = campos[0].trim();
+                    String contraseñaUsuario = campos[1].trim();
+
+                    if (rut.equals(rutUsuario) && contraseña.equals(contraseñaUsuario)) {
+
+                        lector.close();
+                        return true;
+                    }
+                }
+            }
+            lector.close();
+        } catch (IOException ex) {
+            System.err.println("Error al leer el archivo de usuarios: " + ex.getMessage());
+        }
+
+        return false;
+    }
+}
