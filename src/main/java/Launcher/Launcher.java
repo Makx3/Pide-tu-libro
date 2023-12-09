01360ce7a31c08ec083fbfe76b14706982ed2252
Alex Sáez <alexsaezoses@gmail.com>
2023-12-09 02:25:16 -0300

    Commit Launcher: "Launcher"

---
+++ src/main/java/Launcher/Launcher.java
@@ -0,0 +1,11 @@
+package Launcher;
+
+import GUIs.fmrLogin;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        fmrLogin ventana = new fmrLogin();
+        ventana.setVisible(true);
+    }
+}
