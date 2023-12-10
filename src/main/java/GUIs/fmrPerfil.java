4ff46a90b9a3a930018522eda28cad4008c9540b
Makx3 <gatoconb42@gmail.com>
2023-12-10 03:19:12 -0300

    ventana perfil agregada

--- src/main/java/GUIs/fmrPerfil.java
+++ src/main/java/GUIs/fmrPerfil.java
@@ -4,12 +4,19 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class    fmrPerfil extends JFrame {
+public class fmrPerfil extends JFrame {
     private JPanel jpPerfil;
-    private JLabel labInformacion;
-    private JButton botCerrar;
     private JLabel labNombre;
     private JButton botMostrarReservas;
+    private JLabel labPerfil;
+    private JLabel labNombreCargado;
+    private JLabel labApellido;
+    private JLabel labApellidoCargado;
+    private JLabel labEstadoCargado;
+    private JLabel labUltimoLibroReservado;
+    private JLabel labUltimoReservadoCargado;
+    private JLabel labEstado;
+    private JButton botMostrarMenu;
 
     public fmrPerfil() {
         initComponents();
@@ -17,24 +24,21 @@ public class    fmrPerfil extends JFrame {
 
     private void initComponents() {
         setTitle("Perfil");
-        setSize(600, 700);
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(jpPerfil);
 
-        jpPerfil = new JPanel();
-        labInformacion = new JLabel("Aquí va la información del perfil");
-        botCerrar = new JButton("Cerrar Perfil");
-
-        jpPerfil.add(labInformacion);
-        jpPerfil.add(botCerrar);
-
-        add(jpPerfil);
-
-        botCerrar.addActionListener(new ActionListener() {
+        botMostrarMenu.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose();
+                // Aquí puedes abrir la ventana del menú principal
+                fmrMenu ventanaMenu = new fmrMenu(null, null); // Reemplaza null con los parámetros correctos si es necesario
+                ventanaMenu.setVisible(true);
+                dispose(); // Cierra la ventana actual
             }
         });
+
+        // Agrega cualquier otra inicialización o lógica necesaria
     }
 }
